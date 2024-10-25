af0a7a7b1cc433f548b79cfa4d4a73e00e6cd5d0
arekbor <arekindependent@gmail.com>
2024-10-25 12:47:39 +0200

    add pagination vido files

--- src/DataTable/Type/VideoDataTableType.php
+++ src/DataTable/Type/VideoDataTableType.php
@@ -9,6 +9,7 @@ use Kreyu\Bundle\DataTableBundle\Column\Type\ActionsColumnType;
 use Kreyu\Bundle\DataTableBundle\Column\Type\NumberColumnType;
 use Kreyu\Bundle\DataTableBundle\Column\Type\TemplateColumnType;
 use Kreyu\Bundle\DataTableBundle\DataTableBuilderInterface;
+use Kreyu\Bundle\DataTableBundle\Pagination\PaginationData;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class VideoDataTableType extends BaseDataTableType
@@ -58,6 +59,9 @@ class VideoDataTableType extends BaseDataTableType
             ->addFilter('id', NumericFilterType::class, [
                 'label' => 'video.data.table.id.label'
             ]);
+        
+        $builder
+            ->setDefaultPaginationData(new PaginationData(page: 1, perPage: 1));
     }
 
     public function configureOptions(OptionsResolver $resolver): void
