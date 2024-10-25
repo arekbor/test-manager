325f6d2d00a8f843196d43cfcd36119c8955e3ac
arekbor <arekindependent@gmail.com>
2024-10-25 12:22:31 +0200

    fix ui, add tabs

---
+++ assets/controllers/module_details_controller.js
@@ -0,0 +1,39 @@
+import { Controller } from "@hotwired/stimulus";
+import { Tab } from "bootstrap";
+
+export default class ModuleDetailsController extends Controller {
+  static targets = ["tabButton"];
+
+  #localStorageKey = "last-tab";
+  #buttonAttribute = "data-bs-target";
+
+  connect() {
+    this.#activateSavedTab();
+    this.#addTabChangeListener();
+  }
+
+  #activateSavedTab() {
+    const lastTab = localStorage.getItem(this.#localStorageKey);
+    const activeTarget = lastTab
+      ? this.tabButtonTargets.find(
+          (element) => element.getAttribute(this.#buttonAttribute) === lastTab
+        )
+      : this.tabButtonTargets[0];
+
+    if (!activeTarget) {
+      throw new Error("Target element not found.");
+    }
+    new Tab(activeTarget).show();
+  }
+
+  #addTabChangeListener() {
+    this.tabButtonTargets.forEach((element) => {
+      element.addEventListener("click", () => {
+        localStorage.setItem(
+          this.#localStorageKey,
+          element.getAttribute(this.#buttonAttribute)
+        );
+      });
+    });
+  }
+}
