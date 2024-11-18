e5abb64fe997a4b509b12fbe8c477de50d0ddcf1
arekbor <arekindependent@gmail.com>
2024-11-18 14:07:10 +0100

    remove createDataTable from base controller, use trait

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -2,31 +2,12 @@
 
 namespace App\Controller;
 
-use Kreyu\Bundle\DataTableBundle\DataTableFactoryInterface;
-use Kreyu\Bundle\DataTableBundle\DataTableView;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 
 abstract class BaseController extends AbstractController
 {
-    public function __construct(
-        private DataTableFactoryInterface $dataTableFactory,
-        private RequestStack $requestStack
-    ) {
-    }
-
-    protected function createDataTable(string $type, mixed $data = null, array $options = []): DataTableView
-    {
-        $request = $this->requestStack->getCurrentRequest();
-
-        $dataTable = $this->dataTableFactory->create($type, $data, $options);
-        $dataTable->handleRequest($request);
-
-        return $dataTable->createView();
-    }
-
     protected function jsonResponse(string $message, int $status = Response::HTTP_OK): JsonResponse
     {
         return new JsonResponse(['message' => $message], $status);
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,11 +4,14 @@ namespace App\Controller;
 
 use App\DataTable\Type\ModuleDataTableType;
 use App\Repository\ModuleRepository;
+use Kreyu\Bundle\DataTableBundle\DataTableFactoryAwareTrait;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends BaseController
 {
+    use DataTableFactoryAwareTrait;
+
     #[Route('/home')]
     public function index(
         ModuleRepository $moduleRepository
@@ -18,7 +21,7 @@ class HomeController extends BaseController
         $moduleDataTable = $this->createDataTable(ModuleDataTableType::class, $query); 
 
         return $this->render('home/index.html.twig', [
-            'module_data_table' => $moduleDataTable
+            'module_data_table' => $moduleDataTable->createView()
         ]);
     }
 }
