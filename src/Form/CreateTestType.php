76f838758859673154e512f034b40b1ee72bf1eb
arekbor <arekindependent@gmail.com>
2024-12-23 22:04:34 +0100

    add helpers to all create test form and smtp settings

--- src/Form/CreateTestType.php
+++ src/Form/CreateTestType.php
@@ -16,10 +16,15 @@ class CreateTestType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('takerEmail', EmailType::class, [])
+            ->add('takerEmail', EmailType::class, [
+                'label' => 'form.type.createTest.takerEmail.label',
+                'help' => 'form.type.createTest.takerEmail.help',
+            ])
             ->add('expiration', DateTimeType::class, [
                 'widget' => 'single_text',
-                'data' => (new DateTime())->modify('+7 days')
+                'data' => (new DateTime())->modify('+7 days'),
+                'label' => 'form.type.createTest.expiration.label',
+                'help' => 'form.type.createTest.expiration.help',
             ])
             ->add('submit', SubmitType::class, [
                 'label' => 'form.submit.label',
