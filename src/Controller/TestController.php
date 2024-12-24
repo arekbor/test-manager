0bc3f7fa828059fd13d282d11faba8660469112b
arekbor <arekindependent@gmail.com>
2024-12-24 15:30:24 +0100

    add test data table

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -4,14 +4,35 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\DataTable\Type\TestDataTableType;
 use App\Entity\Module;
+use App\Repository\TestRepository;
+use Kreyu\Bundle\DataTableBundle\DataTableFactoryAwareTrait;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/test')]
 class TestController extends AbstractController
 {
+    use DataTableFactoryAwareTrait;
+
+    #[Route('/index')]
+    public function index(
+        Request $request, 
+        TestRepository $testRepository
+    ): Response
+    {
+        $query = $testRepository->createQueryBuilder('t');
+        $testDataTable = $this->createDataTable(TestDataTableType::class, $query);
+        $testDataTable->handleRequest($request);
+
+        return $this->render('test/index.html.twig', [
+            'test_data_table' => $testDataTable->createView()
+        ]);
+    }
+
     #[Route('/create/{id}')]
     public function create(Module $module): Response
     {
---
+++ src/DataTable/Type/TestDataTableType.php
@@ -0,0 +1,69 @@
+<?php 
+
+declare(strict_types=1);
+
+namespace App\DataTable\Type;
+
+use App\Entity\Test;
+use Kreyu\Bundle\DataTableBundle\Action\Type\ButtonActionType;
+use Kreyu\Bundle\DataTableBundle\Bridge\Doctrine\Orm\Filter\Type\NumericFilterType;
+use Kreyu\Bundle\DataTableBundle\Bridge\Doctrine\Orm\Filter\Type\StringFilterType;
+use Kreyu\Bundle\DataTableBundle\Column\Type\ActionsColumnType;
+use Kreyu\Bundle\DataTableBundle\Column\Type\DateTimeColumnType;
+use Kreyu\Bundle\DataTableBundle\Column\Type\NumberColumnType;
+use Kreyu\Bundle\DataTableBundle\Column\Type\TextColumnType;
+use Kreyu\Bundle\DataTableBundle\DataTableBuilderInterface;
+use Kreyu\Bundle\DataTableBundle\Type\AbstractDataTableType;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+class TestDataTableType extends AbstractDataTableType
+{
+    public function __construct(
+        private UrlGeneratorInterface $urlGenerator
+    ) {
+    }
+
+    public function buildDataTable(DataTableBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->addColumn('actions', ActionsColumnType::class, [
+                'label' => 'data_table.actions',
+                'actions' => [
+                    'details' => [
+                        'type' => ButtonActionType::class,
+                        'type_options' => [
+                            'label' => 'data_table.details',
+                            'href' => function(): string {
+                                return $this->urlGenerator->generate('app_home_index', [
+
+                                ]);
+                            }
+                        ]
+                    ]
+                ]
+            ])
+            ->addColumn('id', NumberColumnType::class, [
+                'label' => 'data_table.id'
+            ])
+            ->addColumn('moduleId', NumberColumnType::class, [
+                'label' => 'data_table.test.moduleId',
+                'getter' => fn (Test $test) => $test->getModule()->getId()
+            ])
+            ->addColumn('takerEmail', TextColumnType::class, [
+                'label' => 'data_table.test.takerEmail'
+            ])
+            ->addColumn('expiration', DateTimeColumnType::class, [
+                'label' => 'data_table.test.expiration'
+            ])
+            ->addColumn('submission', DateTimeColumnType::class, [
+                'label' => 'data_table.test.submission'
+            ])
+            ->addFilter('id', NumericFilterType::class, [
+                'label' => 'data_table.id'
+            ])
+            ->addFilter('takerEmail', StringFilterType::class, [
+                'label' => 'data_table.test.takerEmail'
+            ])
+        ;
+    }
+}
