e58aea12c8782f7c0a46959b005032c62a04db88
arekbor <arekindependent@gmail.com>
2025-01-03 23:54:46 +0100

    Remove UniqueLanguages message property

--- src/Validator/UniqueLanguages.php
+++ src/Validator/UniqueLanguages.php
@@ -11,5 +11,5 @@ use Symfony\Component\Validator\Constraint;
 #[\Attribute(\Attribute::TARGET_PROPERTY | \Attribute::TARGET_METHOD | \Attribute::IS_REPEATABLE)]
 class UniqueLanguages extends Constraint
 {
-    public string $message = 'The value "{{ value }}" is not valid.';
+    
 }
