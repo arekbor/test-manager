cfd5e9a99913d5c16df9f2c8ec7b24267324fbfb
arekbor <arekindependent@gmail.com>
2025-01-18 12:55:08 +0100

    return formated messages

--- src/Twig/Extension/TestMessageExtension.php
+++ src/Twig/Extension/TestMessageExtension.php
@@ -11,8 +11,16 @@ class TestMessageExtension extends AbstractExtension
     public function getFunctions(): array
     {
         return [
-            new TwigFunction('introduction_message', [TestMessageRuntime::class, 'getIntroductionMessage']),
-            new TwigFunction('conclusion_message', [TestMessageRuntime::class, 'getConclusionMessage']),
+            new TwigFunction('introduction_message', [TestMessageRuntime::class, 'getIntroductionMessage'], [
+                'is_safe' => [
+                    'html'
+                ]
+            ]),
+            new TwigFunction('conclusion_message', [TestMessageRuntime::class, 'getConclusionMessage'], [
+                'is_safe' => [
+                    'html'
+                ]
+            ]),
         ];
     }
 }
