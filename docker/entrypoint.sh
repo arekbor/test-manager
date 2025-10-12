d644d9f556ac54b68486bbe4bdc58291f8227e1d
arekbor <arekindependent@gmail.com>
2025-10-12 12:39:02 +0200

    run app commands as www-data

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,13 +3,14 @@ set -e
 
 echo "Start entrypoint"
 
-composer install -n
-bin/console cache:clear
-bin/console doctrine:migrations:migrate --no-interaction
-bin/console app:create-app-settings
-bin/console app:create-admin-user
-bin/console importmap:install
-bin/console asset-map:compile
+su -s /bin/bash www-data -c "composer install -n"
+
+su -s /bin/bash www-data -c "bin/console cache:clear"
+su -s /bin/bash www-data -c "bin/console doctrine:migrations:migrate --no-interaction"
+su -s /bin/bash www-data -c "bin/console app:create-app-settings"
+su -s /bin/bash www-data -c "bin/console app:create-admin-user"
+su -s /bin/bash www-data -c "bin/console importmap:install"
+su -s /bin/bash www-data -c "bin/console asset-map:compile"
 
 #this solves the permissions problem after asset-map compilation
 #https://symfony.com/doc/current/setup/file_permissions.html
